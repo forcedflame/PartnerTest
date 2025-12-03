7fcb6f19d75298b32b67c94b2169c39d38387398
Mufaddal Kapadia <161473458+forcedflame@users.noreply.github.com>
2025-12-03 12:16:16 -0500

    Commit 4

--- PTest.java
+++ PTest.java
@@ -1,33 +1,37 @@
 // Hari Rangarajan, Mufaddal Kapadia; 12/3/2025
 import java.util.*;
 import java.io.*;
+import java.util.concurrent.*;
 public class PTest {
 	public static void main(String[] args) {
 		String[] input_files = {"book1.txt", "book2.txt"};
 		String[] output_files = {"book1_output.txt", "book2_output.txt"};
 
-		Thread thread = new Thread(() -> {
-			processFile("book1.txt", "book1_output.txt");
-			processFile("book2.txt", "book2_output.txt");
-		});
+		ExecutorService exe = Executors.newFixedThreadPool(2);
 
+		Future<Double>[] futures = new Future[input_files.length];
 		long start = System.nanoTime();
 
-		thread.start();
+		for(int i = 0; i<futures.length;i++){
+			final int index = i;
+			futures[i] = exe.submit(() -> {
+				long start1 = System.nanoTime();
+				processFile(input_files[index], output_files[index]);
+				long end = System.nanoTime();
 
-		try {
-			thread.join();
-		} catch(Exception e) {
-			e.printStackTrace();
+                return (end - start1) / 1_000_000_000.0;
+			});
 		}
 
-		for(int i = 0; i < input_files.length; i++) {
-			processFile(input_files[i], output_files[i]);
+		for(int i = 0; i<futures.length;i++){
+			try{
+				double threadTime = futures[i].get();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
 		}
-
 		long end = System.nanoTime();
-		double time = (end - start) / 1000000000.0;
-		System.out.println("Total time: " + time + " s");
+		System.out.println("Total time: " + (end - start) / 1000000.0);
 	}
 
 	public static void processFile(String input_file, String output_file) {
@@ -39,6 +43,7 @@ public class PTest {
 			while((line = br.readLine()) != null) {
 				StringBuilder sb = new StringBuilder(line);
 				sb.append(line);
+
 				bw.write(sb.toString());
 				bw.newLine();
 			}
